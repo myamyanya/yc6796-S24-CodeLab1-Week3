cbda7dffed720c55936453d128854e0401232663
VivianC <37090776+myamyanya@users.noreply.github.com>
2024-02-13 03:33:27 -0500

    Add FileIO to the project
    Added FileIO for the project for saving data. Also designed an end scene for the game.

--- Week-3-SomethingAboutFileIO/Assets/Scripts/GameManager.cs
+++ Week-3-SomethingAboutFileIO/Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +14,12 @@ public class GameManager : MonoBehaviour
     public int score = 0;
     public int targetScore = 3;
 
+    // Keys
+    private const string DATA_DIR = "/Data/";
+    private const string DATA_REC_FILE = "record.txt";
+
+    private string DATA_FULL_REC_FILE_PATH;
+    
     // Property
     public int Score
     {
@@ -26,7 +34,7 @@ public class GameManager : MonoBehaviour
             score = value;
             
             // Update the high score
-            // - Why use High not high-
+            // - Why use High not high ?? -
             if (score > HighScore)
             {
                 HighScore = score;
@@ -41,15 +49,37 @@ public class GameManager : MonoBehaviour
     {
         get
         {
+            // if the record exist, pull the data
+            // - Why not coding the "if not" part ?? -
+            if (File.Exists(DATA_FULL_REC_FILE_PATH))
+            {
+                string fileContent = File.ReadAllText(DATA_FULL_REC_FILE_PATH);
+                highScore = Int32.Parse(fileContent);
+            }
+            
             return highScore;
         }
         set
         {
+            Debug.Log("New HighScore!");
+            
             highScore = value;
+
+            // if the directory path is not existing, create one
+            if (!Directory.Exists(Application.dataPath + DATA_DIR))
+            {
+                Directory.CreateDirectory(Application.dataPath + DATA_DIR);
+            }
+            
+            // Write the Highscore to a file
+            // - Why high not High ?? -
+            string fileContent = "" + highScore;
+            File.WriteAllText(DATA_FULL_REC_FILE_PATH, fileContent);
         }
     }
     
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI endText;
 
     private void Awake()
     {
@@ -67,7 +97,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set up full path
+        DATA_FULL_REC_FILE_PATH = Application.dataPath + DATA_DIR + DATA_REC_FILE;
     }
 
     // Update is called once per frame
@@ -76,20 +107,29 @@ public class GameManager : MonoBehaviour
         //Debug.Log(score);
         if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
         {
+            // Unseeing the end scene texts
+            endText.enabled = false;
+            
             scoreText.text = "Score: " + Score + "\n" + 
                              "\nThe firefighting champion put out " + HighScore + " fires!";
         }
         else
         {
+            endText.enabled = true;
+            scoreText.enabled = false;
+            
+            // Unseeing the normal score text
+            
+            
             if (Score >= HighScore)
             {
-                scoreText.text = "You put out " + Score + " fire" + "\n" + 
-                                 "\nYou're the firefighting champion!!";
+                endText.text = "You put out " + Score + " fire" + "\n" + 
+                               "\nYou're the firefighting champion!!";
             }
             else
             {
-                scoreText.text = "You put out " + Score + " fire" + "\n" + 
-                                 "\nThe firefighting champion put out" + HighScore + " fires!";
+                endText.text = "You put out " + Score + " fire" + "\n" + 
+                               "\nThe firefighting champion put out " + HighScore + " fires!";
             }
         }
 
@@ -101,6 +141,16 @@ public class GameManager : MonoBehaviour
             }
 
             targetScore = Mathf.RoundToInt(targetScore + targetScore * 1.5f);
+            
+            Debug.Log(targetScore);
+        }
+        
+        // Press space key to delete high score record
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // This only delete the record file 
+            // ... but not updating the game scene display
+            File.Delete(DATA_FULL_REC_FILE_PATH);
         }
     }
 }
